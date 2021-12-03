07990e339b28c07d403612a84ced34e995780acd
Khannhs <85335920+Khannhs@users.noreply.github.com>
2021-12-03 19:29:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Chị Bống à!',
+    introTitle: 'Cậu à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao chị cứ đi nhầm đường
-    Lạc vào tim em lẻ loi
+    Mà sao cậu cứ đi nhầm đường
+    Lạc vào tim tớ lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích em <33',
-    btnNo: 'Không, em trai à :3',
+    btnYes: 'Vẫn cứ là thích cậu <33',
+    btnNo: 'Không, BẠN à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho em <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Em trêu chị xíu hoi 🥰. Iu chị nhiều nhiều 😘😘',
-    messDesc: 'Thi tốt nha chị Bống xinh đẹp tuyệt vời của em 😘',
-    btnAccept: 'Chúc chị may mắn 🥰 ',
+    mess: 'Trêu xíu hoi 🥰. Iu chị nhiều nhiều 😘😘',
+    messDesc: 'Học hóa đi nhé 😘',
+    btnAccept: 'Pai pai 🥰 ',
     
 }
