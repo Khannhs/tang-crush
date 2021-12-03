0ec5bb4eff438f29a0be4ccf73bc377a32388f3f
Khannhs <85335920+Khannhs@users.noreply.github.com>
2021-12-03 19:31:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ const CONFIG = {
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho em <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Trêu xíu hoi 🥰. Iu chị nhiều nhiều 😘😘',
+    mess: 'Trêu xíu hoi 🥰😘😘',
     messDesc: 'Học hóa đi nhé 😘',
     btnAccept: 'Pai pai 🥰 ',
     
